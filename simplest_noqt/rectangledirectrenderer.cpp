19765e9b330f22cea6014579dc910fa4303cb730
Keith Kyzivat <kyzivat_k@l-3com.com>
2017-05-18 18:17:09 -0400

    Base app does nothing.

---
+++ simplest_noqt/rectangledirectrenderer.cpp
@@ -0,0 +1,6 @@
+#include "rectangledirectrenderer.h"
+
+RectangleDirectRenderer::RectangleDirectRenderer()
+{
+
+}
---
+++ simplest_noqt/rectangledirectrenderer.h
@@ -0,0 +1,11 @@
+#ifndef RECTANGLEDIRECTRENDERER_H
+#define RECTANGLEDIRECTRENDERER_H
+
+
+class RectangleDirectRenderer
+{
+public:
+    RectangleDirectRenderer();
+};
+
+#endif // RECTANGLEDIRECTRENDERER_H
